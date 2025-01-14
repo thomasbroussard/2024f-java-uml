ef80acdb853be9f15a6e64a1e5e2cd05ff8fd332
Présentation <tbr@tbr.com>
2025-01-14 15:40:19 +0100

    introduce spring boot

--- java-uml/biostat/src/main/java/fr/epita/biostat/Application.java
+++ java-uml/biostat/src/main/java/fr/epita/biostat/Application.java
@@ -1,12 +1,18 @@
 package fr.epita.biostat;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8080"));
+       app.run(args);
     }
 }
