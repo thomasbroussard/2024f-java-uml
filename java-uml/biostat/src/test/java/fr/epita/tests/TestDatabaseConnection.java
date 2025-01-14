40a008b358d704db27d29a1600b138eaf8b3e93c
Présentation <tbr@tbr.com>
2025-01-14 09:50:41 +0100

    parameterized statements

--- java-uml/biostat/src/test/java/fr/epita/tests/TestDatabaseConnection.java
+++ java-uml/biostat/src/test/java/fr/epita/tests/TestDatabaseConnection.java
@@ -26,12 +26,27 @@ public class TestDatabaseConnection {
         PreparedStatement insertStatement = connection.prepareStatement(sqlInsert);
         insertStatement.execute();
 
+        String sqlParamInsert =
+                "INSERT INTO PERSONS(name, gender, age, height, weight)" +
+                " VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement insertParamStatement = connection.prepareStatement(sqlParamInsert);
+        insertParamStatement.setString(1, "test2");
+        insertParamStatement.setString(2, "F");
+        insertParamStatement.setInt(3, 24);
+        insertParamStatement.setInt(4, 167);
+        insertParamStatement.setInt(5, 60);
+        insertParamStatement.execute();
+
         String sqlSelect =
-                "SELECT * FROM PERSONS";
+                "SELECT name, gender, age, height, weight FROM PERSONS";
         PreparedStatement selectStatement = connection.prepareStatement(sqlSelect);
         ResultSet resultSet = selectStatement.executeQuery();
         while (resultSet.next()){
             System.out.println(resultSet.getString("name"));
+            System.out.println(resultSet.getString("gender"));
+            System.out.println(resultSet.getInt("age"));
+            System.out.println(resultSet.getInt("height"));
+            System.out.println(resultSet.getInt("weight"));
         }
 
         connection.close();
